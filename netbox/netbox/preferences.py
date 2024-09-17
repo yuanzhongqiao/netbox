6d0a3485a11ba7a74dff0553758508515f0975af
Arthur Hanson <worldnomad@gmail.com>
2024-09-17 13:16:09 -0700

    17522 enable localization of user preference form

--- netbox/netbox/preferences.py
+++ netbox/netbox/preferences.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 from netbox.registry import registry
 from users.preferences import UserPreference
